79ffab28fe00b69236d6c31429b333ecd71a0bfd
laoma <546772165@qq.com>
2019-11-16 09:10:45 +0800

    对密码进行MD5的加密操作—By LaoMa

--- src/main/java/top/oddou/blog/service/UserServiceImpl.java
+++ src/main/java/top/oddou/blog/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.oddou.blog.dao.UserRepository;
 import top.oddou.blog.po.User;
+import top.oddou.blog.util.MD5Util;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User userCheck(String userName, String password) {
-        User  user = userRepository.findByUserNameAndPassword(userName,password);
+        User  user = userRepository.findByUserNameAndPassword(userName, MD5Util.code(password));
         return user;
     }
 }
